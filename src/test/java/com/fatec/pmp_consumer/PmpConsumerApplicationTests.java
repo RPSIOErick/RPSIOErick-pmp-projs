8633c7f95f29460e2df4fcbe22ecd8baa5209498
RPSIOErick <ecerickbastos@gmail.com>
2025-12-03 17:08:44 -0300

    Add: Modulo 4

---
+++ src/test/java/com/fatec/pmp_consumer/PmpConsumerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.erick.pmp_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmpConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+        //Não há testes
+	}
+
+}
